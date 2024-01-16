354ff577320caa69beccaf4b6b1671e206f344b7
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2024-01-16 15:30:43 +0300

    1.0v

---
+++ ITetragonchiki/ITetragonchiki/Program.cs
@@ -0,0 +1,97 @@
+﻿public interface ITetragon
+{
+    public float CountPerimeter(float a, float b, float c = 0, float d = 0);
+
+    public float CountArea(float a, float b, float angle);
+}
+
+public class ConvexTetragon : ITetragon
+{
+    public float CountPerimeter(float a, float b, float c = 0, float d = 0)
+    {
+        return a + b + c + d;
+    }
+
+    public float CountArea(float a, float b, float angle)
+    {
+        return a * b / 2 * (float)Math.Sin(angle);
+    }
+}
+
+public class Parallelogram : ITetragon
+{
+    public float CountPerimeter(float a, float b, float c = 0, float d = 0)
+    {
+        return 2 * (a + b);
+    }
+
+    public float CountArea(float a, float b, float angle = 0)
+    {
+        return a * b; //сторона на высоту
+    }
+}
+
+public class Rhombus : ITetragon
+{
+    public float CountPerimeter(float a, float b = 0, float c = 0, float d = 0)
+    {
+        return 4 * a;
+    }
+
+    public float CountArea(float a, float b, float angle = 0)
+    {
+        return a * b / 2; //диагонали на 1/2
+    }
+}
+
+public class Rectangle : ITetragon
+{
+    public float CountPerimeter(float a, float b, float c = 0, float d = 0)
+    {
+        return 2 * (a + b);
+    }
+
+    public float CountArea(float a, float b, float angle = 0)
+    {
+        return a * b; //сторона на сторону
+    }
+}
+
+public class Square : ITetragon
+{
+    public float CountArea(float a, float b = 0, float angle = 0)
+    {
+        return (float)Math.Pow(a, 2); //сторона на сторону
+    }
+
+    public float CountPerimeter(float a, float b = 0, float c = 0, float d = 0)
+    {
+        return 4 * a;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ITetragon[] tetragons = new ITetragon[]
+        {
+            new ConvexTetragon(),
+            new Parallelogram(),
+            new Rhombus(),
+            new Rectangle(),
+            new Square()
+        };
+
+        foreach (var tetragon in tetragons)
+        {
+            float perimeter = tetragon.CountPerimeter(4, 4);
+            float area = tetragon.CountArea(4, 4, (float)Math.PI / 2);
+
+            Console.WriteLine(tetragon.GetType().Name);
+            Console.WriteLine("Perimeter: " + perimeter);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Tetragonchiki/Tetragonchiki/Program.cs
@@ -0,0 +1,82 @@
+﻿public abstract class ATetragon
+{
+    public virtual float CountPerimeter(float a, float b, float c = 0, float d = 0)
+    {
+        return a + b + c + d;
+    }
+
+    public virtual float CountArea(float a, float b, float angle)
+    {
+        return a * b / 2 * (float)Math.Sin(angle); //диагонали на 1/2 и на синус угла
+    }
+}
+
+public class ConvexTetragon : ATetragon
+{
+
+}
+
+public class Parallelogram : ConvexTetragon
+{
+    public override float CountArea(float a, float b, float angle = 0)
+    {
+        return a * b; //сторона на высоту
+    }
+
+    public override float CountPerimeter(float a, float b, float c = 0, float d = 0)
+    {
+        return 2 * (a + b);
+    }
+}
+
+public class Rhombus : Parallelogram
+{
+    public override float CountArea(float a, float b, float angle = 0)
+    {
+        return a * b / 2; //диагонали на 1/2
+    }
+
+    public override float CountPerimeter(float a, float b = 0, float c = 0, float d = 0)
+    {
+        return 4 * a;
+    }
+}
+
+public class Rectangle : Parallelogram
+{
+    //площадь та же, но сторона на сторону
+}
+
+public class Square : Rhombus
+{
+    public override float CountArea(float a, float b = 0, float angle = 0)
+    {
+        return (float)Math.Pow(a, 2); //сторона на сторону
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ATetragon[] tetragons = new ATetragon[]
+        {
+            new ConvexTetragon(),
+            new Parallelogram(),
+            new Rhombus(),
+            new Rectangle(),
+            new Square()
+        };
+
+        foreach (var tetragon in tetragons)
+        {
+            float perimeter = tetragon.CountPerimeter(4, 4);
+            float area = tetragon.CountArea(4, 4, (float)Math.PI / 2);
+
+            Console.WriteLine(tetragon.GetType().Name);
+            Console.WriteLine("Perimeter: " + perimeter);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine();
+        }
+    }
+}
